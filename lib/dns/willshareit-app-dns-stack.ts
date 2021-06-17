6c3745f1a5ff84276d7fca6a41ed01240cd8b3c1
PRAMOD A JINGADE <avj2352@gmail.com>
2021-06-17 08:48:09 +0800

    added DNS stack

--- lib/dns/willshareit-app-dns-stack.ts
+++ lib/dns/willshareit-app-dns-stack.ts
@@ -1,15 +1,33 @@
+import { Certificate, CertificateValidation, ICertificate } from '@aws-cdk/aws-certificatemanager';
+import { IPublicHostedZone, PublicHostedZone } from '@aws-cdk/aws-route53';
 import * as cdk from '@aws-cdk/core';
 
+interface IWillshareitAppDnsStackProps extends cdk.StackProps {
+  dnsName: string;
+}
+
 /**
- * PAJ - Main Stack to define
- * Domain
- * HostedZone
- * CertificateManager
+ * PAJ - Code for managing DNS, Hosted Zone & Certificate
  */
 export class WillshareitAppDnsStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  private readonly _hostedZone: IPublicHostedZone;
+  private readonly _certificate: ICertificate;
+
+  constructor(scope: cdk.Construct, id: string, props: IWillshareitAppDnsStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    this._hostedZone = new PublicHostedZone(this, 'WillshareitAppHostedZone', {
+      zoneName: props.dnsName
+    });
+
+    this._certificate = new Certificate(this, 'WillshareitAppCertificateManager', {
+      domainName: props.dnsName,
+      validation: CertificateValidation.fromDns(this.hostedZone),
+    });
+
   }
+  // getter - _hostedZone
+  public get hostedZone() { return this._hostedZone; }
+  // getter - _certificate
+  public get certificate() { return this._certificate; }
 }
